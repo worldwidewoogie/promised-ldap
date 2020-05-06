47e19250cf2e66e8fb2669e857668d14b0c7fc9a
Mike Wohlgemuth <m.wohlgemuth@northeastern.edu>
2020-05-06 09:35:11 -0400

    handle connection layer errors

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 var ldap = require('ldapjs');
 var Promise = require('any-promise');
 
+var connectionError = null
 
 function Client(options) {
   this.client = ldap.createClient(options);
+
+  this.client.on('error', function(err) {
+    console.dir(err);
+    connectionError = err
+  })
 }
 
 
@@ -41,9 +47,14 @@ Client.prototype.search = function (base, options, controls) {
     var searchCallback = function (err, result) {
       var r = {
         entries: [],
-        hitSizeLimit: false,
         references: []
       };
+
+      if (connectionError) {
+        let e = connectionError
+        let connectionError = null
+        reject(e)
+      }
 
       result.on('searchEntry', function (entry) {
         r.entries.push(entry);
@@ -54,12 +65,6 @@ Client.prototype.search = function (base, options, controls) {
       });
 
       result.on('error', function (err) {
-        if (options.sizeLimit && err.name === 'SizeLimitExceededError') {
-          // Swallow error on limiting result size if we are limited
-          // See https://github.com/mcavage/node-ldapjs/issues/236
-          r.hitSizeLimit = true
-          resolve(r);
-        }
         reject(err);
       });
 
